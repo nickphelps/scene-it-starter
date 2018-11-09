48c344d2dc512b9ba3421ed750b7ba16f98a29c4
Nick Phelps <Nick90052@Gmail.com>
2018-11-09 15:50:46 -0600

    Scene It Complete

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-
-    //reset watchlist
-    // localStorage.setItem('watchlist', '')
-    
-    function renderMovies (movieData) {
-        let movieHTML = movieData.map(function (currentMovie) {
-            console.log(currentMovie)
-            let movieStringHTML = `
+  function renderMovies (movieData) {
+    let movieHTML = movieData.map(function (currentMovie) {
+      console.log(currentMovie)
+      let movieStringHTML = `
             <div class="card bg-dark text-white">
                 <img class="card-img-top" src="${currentMovie.Poster}" alt="${currentMovie.Title}">
                 <div class="card-body">
@@ -17,55 +13,45 @@ document.addEventListener('DOMContentLoaded', function () {
                     <div>
                         <h5 class="movie-year">${currentMovie.Year}</h5>
                     </div>
-                    <a onclick="saveToWatchlist('${currentMovie.imdbID}')" href="#" class="btn btn-primary">Add!</a>
+                    <button onclick="saveToWatchlist('${currentMovie.imdbID}')" type="button" class="btn btn-primary btn-lg btn-block">Add to Watchlist</button>
                 </div>
             </div>
             `
-            return movieStringHTML
-        })//map function
-        return movieHTML.join('')
-    }//render movies
+      return movieStringHTML
+    })// map function
+    return movieHTML.join('')
+  }// render movies
 
-    document.getElementById('search-form').addEventListener('input', function(e) {
-        e.preventDefault()
-
-        let searchString = $('.search-bar').val().toLowerCase()
-        //formats searchstring to acceptable url format
-        let urlEncodedSearchString = encodeURIComponent(searchString)
-        //passing searchstring into API
-        axios.get("http://www.omdbapi.com/?apikey=3430a78&s=" + urlEncodedSearchString)
-            .then(function (response) {
-                console.log(response.data)
-                //rendering movies to movie container
+  document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault()
+    let searchString = $('.search-bar').val().toLowerCase()
+    // formats searchstring to acceptable url format
+    let urlEncodedSearchString = encodeURIComponent(searchString)
+    // passing searchstring into API
+    axios.get('http://www.omdbapi.com/?apikey=3430a78&s=' + urlEncodedSearchString)
+      .then(function (response) {
+        console.log(response.data)
+        // rendering movies to movie container
         $('#movies-container').html('<div class="card-columns">' + renderMovies(response.data.Search) + '</div>')
-                movieData = response.data.Search
-        // return responseMovieData
-        
-            })//then
-            
-        // let filteredData = movieData.filter(function (currentMovie) {
-        //     let foundInTitle = currentMovie.Title.toLowerCase().indexOf(searchString) > -1
-        //     return foundInTitle
-        // })//filteredData
+        // updating movie data for movies added to watchlist
+        movieData = response.data.Search
+      })// then
+  })// form event listener
+})// DOM Content Loaded
 
-    })//form event listener
+function saveToWatchlist (imdbID) {
+  let movie = movieData.find(function (currentMovie) {
+    return currentMovie.imdbID == imdbID
+  })
 
-})//DOM Content Loaded
+  let watchlistJSON = localStorage.getItem('watchlist')
+  let watchlist = JSON.parse(watchlistJSON)
+  if (watchlist == null) {
+    watchlist = []
+  } // if statement
+  watchlist.push(movie)
+  watchlistJSON = JSON.stringify(watchlist)
+  localStorage.setItem('watchlist', watchlistJSON)
 
-function saveToWatchlist(imdbID) {
-    let movie = movieData.find(function(currentMovie){
-        return currentMovie.imdbID == imdbID
-    })
-    // console.log(imdbID)
-    let watchlistJSON = localStorage.getItem('watchlist')
-    let watchlist = JSON.parse(watchlistJSON)
-    if (watchlist == null) {
-        watchlist = []
-    } //if statement
-        watchlist.push(movie)
-        watchlistJSON = JSON.stringify(watchlist)
-        localStorage.setItem('watchlist', watchlistJSON)
-      
-    console.log(watchlist)
-}//saveToWatchList
-
+  console.log(watchlist)
+}// saveToWatchList
--- watchlist.js
+++ watchlist.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let myWatchList = localStorage.getItem('watchlist')
-    let myWatchListJSON = JSON.parse(myWatchList)
-    console.log(myWatchListJSON)
-    $('#movies-container').html('<div class="card-columns">' + renderMovies(myWatchListJSON) + '</div>')
-    function renderMovies (movieArray) {
-        let movieHTML = movieArray.map(function (myWatchListJSON) {
-            let movieStringHTML = `
+  let myWatchList = localStorage.getItem('watchlist')
+  let myWatchListJSON = JSON.parse(myWatchList)
+
+  movieData = myWatchListJSON
+
+  $('#movies-container').html('<div class="card-columns">' + renderMovies(myWatchListJSON) + '</div>')
+
+  function renderMovies (movieArray) {
+    console.log(movieArray)
+    let movieHTML = movieArray.map(function (myWatchListJSON) {
+      console.log(myWatchListJSON)
+      let movieStringHTML = `
             <div class="card bg-dark text-white">
                 <img class="card-img-top" src="${myWatchListJSON.Poster}" alt="${myWatchListJSON.Title}">
                 <div class="card-body">
@@ -15,16 +20,32 @@ document.addEventListener('DOMContentLoaded', function () {
                     <div>
                         <h5 class="movie-year">${myWatchListJSON.Year}</h5>
                     </div>
-                    <a onclick="saveToWatchlist('${myWatchListJSON.imdbID}')" href="#" class="btn btn-primary">Remove!</a>
+                    <button onclick="removeFromWatchlist('${myWatchListJSON.imdbID}')" type="button" class="btn btn-primary btn-lg btn-block">Remove from Watchlist</button>
                 </div>
             </div>
             `
-            return movieStringHTML
-            
-        })//map function
-        return movieHTML.join('')
-    }//render movies
+      return movieStringHTML
+    })// map function
+    return movieHTML.join('')
+  }// render movies
 })
 
+function removeFromWatchlist (imdbID) {
+  // clicked on movie data is stored to movie
 
+  let watchlistJSON = localStorage.getItem('watchlist')
+  let watchlist = JSON.parse(watchlistJSON)
+  console.log(watchlist)
+  if (watchlist == null) {
+    watchlist = []
+  } // if statement
+  // create new array for all elements in watchlist that do not have selected imdbID
+  watchlist = watchlist.filter(function (movie) {
+    return movie.imdbID !== imdbID
+  })// watchlist filter
 
+  watchlistJSON = JSON.stringify(watchlist)
+  localStorage.setItem('watchlist', watchlistJSON)
+
+  $('#movies-container').html('<div class="card-columns">' + renderMovies(watchlist) + '</div>')
+}// remove from Watch List
